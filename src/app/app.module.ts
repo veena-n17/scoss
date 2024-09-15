27e1b4b27484decdcb7b6f9ffad8183c5602c011
Richard <richrd77@gmail.com>
2024-09-16 01:10:13 +0530

    few stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ScossMatModule } from './common/mat/mat.module';
+import { ScossCommonModule } from './common/common.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ScossMatModule } from './common/mat/mat.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ScossMatModule
+    ScossMatModule,
+    ScossCommonModule
   ],
   providers: [
     provideAnimationsAsync()
